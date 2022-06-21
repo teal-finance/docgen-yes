f8f332505c18aca8c684c1ed36d470402060a9a5
Oliver <0uep@hessling.fr>
2022-06-21 12:51:20 +0200

    ✅ Add test cases and parallelize tests

--- builder_test.go
+++ builder_test.go
@@ -1,74 +1,99 @@
-package docgen
+package docgen_test
 
 import (
+	"net/http"
 	"reflect"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
+
+	"github.com/teal-finance/docgen-yes"
 )
 
 func TestBuildDoc(t *testing.T) {
-	type args struct {
-		r chi.Routes
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    Doc
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := BuildDoc(tt.args.r)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("BuildDoc() error = %v, wantErr %v", err, tt.wantErr)
+	t.Parallel()
 
+	cases := []struct {
+		name    string
+		routes  chi.Routes
+		want    docgen.Doc
+		wantErr bool
+	}{{
+		name:    "empty",
+		routes:  nil,
+		want:    docgen.Doc{},
+		wantErr: false,
+	}}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := docgen.BuildDoc(c.routes)
+			if (err != nil) != c.wantErr {
+				t.Errorf("BuildDoc() error = %v, wantErr %v", err, c.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("BuildDoc() = %v, want %v", got, tt.want)
+
+			if !reflect.DeepEqual(got, c.want) {
+				t.Errorf("BuildDoc() = %v, want %v", got, c.want)
 			}
 		})
 	}
 }
 
 func Test_buildDocRouter(t *testing.T) {
-	type args struct {
-		r chi.Routes
-	}
-	tests := []struct {
-		name string
-		args args
-		want DocRouter
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := buildDocRouter(tt.args.r); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("buildDocRouter() = %v, want %v", got, tt.want)
+	t.Parallel()
+
+	cases := []struct {
+		name   string
+		routes chi.Routes
+		want   docgen.DocRouter
+	}{{
+		name:   "empty",
+		routes: nil, want: docgen.DocRouter{},
+	}}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := docgen.BuildDocRouter(c.routes); !reflect.DeepEqual(got, c.want) {
+				t.Errorf("buildDocRouter() = %v, want %v", got, c.want)
 			}
 		})
 	}
 }
 
+type next struct{ called bool }
+
+func (n *next) ServeHTTP(http.ResponseWriter, *http.Request) { n.called = true }
+
 func Test_GetFuncInfo(t *testing.T) {
-	type args struct {
-		i interface{}
-	}
-	tests := []struct {
+	t.Parallel()
+
+	cases := []struct {
 		name string
-		args args
-		want FuncInfo
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetFuncInfo(tt.args.i); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetFuncInfo() = %v, want %v", got, tt.want)
+		i    interface{}
+		want docgen.FuncInfo
+	}{{
+		name: "Unresolvable",
+		i:    next{},
+		want: docgen.FuncInfo{Unresolvable: true},
+	}}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := docgen.GetFuncInfo(c.i); !reflect.DeepEqual(got, c.want) {
+				t.Errorf("GetFuncInfo() = %v, want %v", got, c.want)
 			}
 		})
 	}
